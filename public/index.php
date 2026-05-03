36e9a1912f33e0c318ef65e597f8b3b02f2fd952
Peds Yorpo <pedroyorpo17@gmail.com>
2026-05-03 15:45:06 +0800

    commit

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@ define('LARAVEL_START', microtime(true));
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
-
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
